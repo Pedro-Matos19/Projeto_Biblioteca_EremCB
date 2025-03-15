5ef2e89c51f7237c3aabccfd53410aae198671de
Pedro Henrique <pedrohenriquemo2020@gmail.com>
2025-03-15 15:24:37 -0300

    feat: add fireBase/primeiros components/service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB80f0VyL5L1dBQm25LZXOnWJcEwBEyT2U',
+  authDomain: 'biblioteca-4aefe.firebaseapp.com',
+  projectId: 'biblioteca-4aefe',
+  storageBucket: 'biblioteca-4aefe.firebasestorage.app',
+  messagingSenderId: '854013119745',
+  appId: '1:854013119745:web:fbb0e9c552fa10e17e5f00',
+};
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+              provideRouter(routes),
+              provideFirebaseApp(() => initializeApp(firebaseConfig)),
+              provideFirestore(() => getFirestore())
+            ]
 };
---
+++ src/app/component/list-livros/list-livros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-livros',
+  imports: [],
+  templateUrl: './list-livros.component.html',
+  styleUrl: './list-livros.component.scss'
+})
+export class ListLivrosComponent {
+
+}
