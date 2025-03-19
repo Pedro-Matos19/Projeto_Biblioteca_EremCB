e158b71ac8aec7688537f90d2772ed36cc7bdc6f
Pedro Henrique <pedrohenriquemo2020@gmail.com>
2025-03-19 11:27:01 -0300

    feat: melhoria no service

--- src/app/service/livros-service.service.ts
+++ src/app/service/livros-service.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, Firestore, getDocs, getFirestore } from '@angular/fire/firestore';
+import { Livro } from '../types';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivrosServiceService {
 
-  constructor() { }
+
+constructor() {}
+private firestore = inject(Firestore);
+
+livrosCollection = collection(this.firestore, 'Livros');
+
+async addBook(livro: Livro){
+  const ref = collection(this.firestore, 'Livros')
+  return addDoc(ref, livro);
+}
+
+  getAllBooks(): Observable<Livro[]> {
+   return collectionData(this.livrosCollection, {idField: 'id'}) as Observable<Livro[]>
+  }
 }
